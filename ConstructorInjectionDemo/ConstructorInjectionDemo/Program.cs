424d0bb048d8ec64b1fef11ae8dbbd768e61e302
karlinskid <akarlisky@msn.com>
2015-03-10 12:50:08 -0600

    Initial Commit - Project added.

---
+++ ConstructorInjectionDemo/ConstructorInjectionDemo/Program.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+namespace ConstructorInjectionDemo
+{
+    /// <summary>
+    /// This application is an example of Constructor Injection to have 
+    /// loose coupling.
+    /// </summary>
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            int numberOfPassengers;
+            string passengerInput;
+
+            do
+            {
+                System.Console.WriteLine( "Please enter number of passengers" );
+                passengerInput = Console.ReadLine();
+                Console.WriteLine( "\n" );
+
+            } while (!Int32.TryParse( passengerInput, out numberOfPassengers ));
+
+            DecideVehicle( numberOfPassengers );
+
+            Console.WriteLine( "Press any key to exit." );
+            Console.ReadKey();
+
+        }
+
+        private static void DecideVehicle( int numberOfPassengers )
+        {
+            Driver driver;
+
+            if (numberOfPassengers == 0)
+            {
+                Console.WriteLine( "Cannot go without a passenger." );
+                return;
+            }
+            else if (numberOfPassengers == 1)
+            {
+                var avendator = new Avendator();
+                driver = new Driver( avendator );
+            }
+            else if (numberOfPassengers == 1)
+            {
+                var civic = new Civic();
+                driver = new Driver( civic );
+            }
+            else if (numberOfPassengers == 1)
+            {
+                var sienna = new Sienna();
+                driver = new Driver( sienna );
+            }
+            else
+            {
+                Console.WriteLine( "Too many passengers." );
+                return;
+            }
+        }
+    }
+
+    class Driver
+    {
+        ICar _car;
+
+        // Injecting ICar interface to constructor
+        public Driver( ICar car )
+        {
+            _car = car;
+        }
+    }
+
+    class Avendator : ICar
+    {
+        public void Drive( int numberOfPassengers )
+        {
+            Console.WriteLine( "Driving a an Lamborghini Avendator with {0} passeger.",
+                                numberOfPassengers );
+        }
+    }
+
+    class Civic : ICar
+    {
+        public void Drive( int numberOfPassengers )
+        {
+            Console.WriteLine( "Driving a an Honda Civic Avendator with {0} passegers.",
+                    numberOfPassengers );
+        }
+    }
+
+    class Sienna : ICar
+    {
+        public void Drive( int numberOfPassengers )
+        {
+            Console.WriteLine( "Driving a an Toyota Sienna with {0} passegers.",
+                    numberOfPassengers );
+        }
+    }
+
+    interface ICar
+    {
+        void Drive( int numberOfPassengers );
+    }
+}
